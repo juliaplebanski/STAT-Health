0294814c172285d7fe7a7c768e95f389f17ab2fb
Zainab Dialloe <zainabdiallo21@gmail.com>
2021-04-06 13:12:24 -0400

    Update

--- java/src/main/java/com/techelevator/dao/JDBCVisitDAO.java
+++ java/src/main/java/com/techelevator/dao/JDBCVisitDAO.java
@@ -1,5 +1,35 @@
 package com.techelevator.dao;
 
-public class JDBCVisitDAO {
+import java.time.LocalDate;
+import java.util.List;
 
+import javax.sql.DataSource;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import com.techelevator.model.Visit;
+
+public class JDBCVisitDAO implements VisitDAO{
+	
+	private JdbcTemplate jdbcTemplate;
+
+	public JDBCVisitDAO(DataSource dataSource) {
+		this.jdbcTemplate = new JdbcTemplate(dataSource);
+	}
+	
+	
+
+	@Override
+	public List<Visit> getAllAvailableVisitsByDoctorId(int doctorId, LocalDate dateOfVisit) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	
+	
+	
+	
+	
+	
+	
 }
