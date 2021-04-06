8682f05204fd4a38db39cbbec0f9e60c1e5ad952
Julia Plebanski <julplebanski@gmail.com>
2021-04-06 09:13:32 -0400

    added more visits

--- java/database/schema.sql
+++ java/database/schema.sql
@@ -1,5 +1,9 @@
 BEGIN TRANSACTION;
 
+DROP TABLE IF EXISTS visit;
+DROP TABLE IF EXISTS visit_status;
+DROP TABLE IF EXISTS patient;
+DROP TABLE IF EXISTS doctor;
 DROP TABLE IF EXISTS users;
 DROP SEQUENCE IF EXISTS seq_user_id;
 
@@ -18,8 +22,55 @@ CREATE TABLE users (
 	CONSTRAINT PK_user PRIMARY KEY (user_id)
 );
 
+CREATE TABLE patient (
+  patient_id serial NOT NULL,
+  user_id integer NOT NULL,
+  CONSTRAINT pk_patient_id PRIMARY KEY (patient_id),
+  CONSTRAINT fk_user_id FOREIGN KEY (user_id) REFERENCES users(user_id)
+);
+CREATE TABLE doctor (
+  doctor_id serial NOT NULL,
+  user_id integer NOT NULL,
+  first_name varchar (50) NOT NULL,
+  last_name varchar (50) NOT NULL,
+  CONSTRAINT pk_doctor_id PRIMARY KEY (doctor_id),
+  CONSTRAINT fk_user_id FOREIGN KEY (user_id) REFERENCES users(user_id)
+);
+
+CREATE TABLE visit_status (
+  status_id char(1) NOT NULL,
+  status_desc varchar(50) NOT NULL,
+  CONSTRAINT pk_status_id PRIMARY KEY (status_id)
+);
+
+CREATE TABLE visit (
+  visit_id serial NOT NULL,
+  patient_id integer NOT NULL,
+  doctor_id integer NOT NULL,
+  date_of_visit date NOT NULL,
+  start_time time NOT NULL,
+  end_time time NOT NULL,
+  status_id char(1) NOT NULL,
+  CONSTRAINT pk_visit_id PRIMARY KEY (visit_id),
+  CONSTRAINT fk_patient_id FOREIGN KEY (patient_id) REFERENCES patient(patient_id),
+  CONSTRAINT fk_doctor_id FOREIGN KEY (doctor_id) REFERENCES doctor(doctor_id),
+  CONSTRAINT fk_status_id FOREIGN KEY (status_id) REFERENCES visit_status(status_id)
+);
+
+
 INSERT INTO users (username,password_hash,role) VALUES ('user','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_USER');
 INSERT INTO users (username,password_hash,role) VALUES ('admin','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_ADMIN');
+
+INSERT INTO patient (user_id) VALUES (1);
+INSERT INTO patient (user_id) VALUES (1);
+INSERT INTO doctor (user_id,first_name,last_name) VALUES (2, 'Steve', 'Carmicheal');
+INSERT INTO visit_status (status_id,status_desc) VALUES ('a', 'approved');
+INSERT INTO visit_status (status_id, status_desc) VALUES ('p','pending');
+INSERT INTO visit (patient_id,doctor_id, date_of_visit, start_time, end_time, status_id) VALUES (1,1, '5/5/2021', '8:00', '8:30', 'a');
+INSERT INTO visit (patient_id,doctor_id, date_of_visit, start_time, end_time, status_id) VALUES (1,1, '5/9/2021', '9:00', '9:30', 'a');
+INSERT INTO visit (patient_id,doctor_id, date_of_visit, start_time, end_time, status_id) VALUES (1,1, '5/12/2021', '9:00', '9:30', 'a');
+INSERT INTO visit (patient_id,doctor_id, date_of_visit, start_time, end_time, status_id) VALUES (1,1, '5/16/2021', '8:00', '8:30', 'a');
+
 
 
 COMMIT TRANSACTION;
