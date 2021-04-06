b8f3045807aaa8f784949dc00aa9093217b2391b
Julia Plebanski <julplebanski@gmail.com>
2021-04-06 15:55:54 -0400

    updated visit dates

--- java/src/main/java/com/techelevator/controller/VisitController.java
+++ java/src/main/java/com/techelevator/controller/VisitController.java
@@ -8,12 +8,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.techelevator.model.Visit;
-
 
 @RestController
 @CrossOrigin
@@ -22,28 +22,15 @@ import com.techelevator.model.Visit;
 //Mapping starts here
 @RequestMapping(path = "/visit")
 public class VisitController {
-	
-	
+
 	@Autowired
 	VisitDAO visitDao;
 
-	
-	
 //	Mapped as a GET Method using base url of /visit
 	@GetMapping()
-    List<Visit>getAllAvailableVisitsByDoctorId(@RequestBody int doctorId, LocalDate dateOfVisit)
-	{
+	public List<Visit> getAllAvailableVisitsByDoctorId(@RequestBody @PathVariable int doctorId, LocalDate dateOfVisit) {
 		return visitDao.getAllAvailableVisitsByDoctorId(doctorId, dateOfVisit);
-		
+
 	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
 
 }
